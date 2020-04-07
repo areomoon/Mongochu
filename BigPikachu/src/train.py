be7dd0b6cdcd021c884cf6ca3f953975856117b2
areomoon <tsengyangyu@gmail.com>
2020-04-07 17:24:18 +0800

    aug

--- BigPikachu/src/train.py
+++ BigPikachu/src/train.py
@@ -62,7 +62,7 @@ parser.add_argument('--save_dir', default='../weights', type=str,
 args = parser.parse_args()
 
 
-LSLoss = LabelSmoothingLoss(3, smoothing=0.1)
+LSLoss = LabelSmoothingLoss(3, smoothing=0.3)
 
 def loss_fn(outputs,target):
     loss = nn.CrossEntropyLoss()(outputs, target)
