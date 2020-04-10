a38f395eb66d5a41e44912617e4ff9d6a43f59a8
Tang-Li-Jen <b00701160@gmail.com>
2020-04-11 01:04:37 +0800

    change model to se-resnet

--- BigPikachu/src/models.py
+++ BigPikachu/src/models.py
@@ -2,22 +2,22 @@ import torch.nn as nn
 import pretrainedmodels
 from torch.nn import functional as F
 
-# class ResNet34(nn.Module):
-#     def __init__(self,pretrained, n_class):
-#         super(ResNet34, self).__init__()
-#         if pretrained is True:
-#             self.model = pretrainedmodels.__dict__["resnet34"](pretrained='imagenet')
-#         else:
-#             self.model = pretrainedmodels.__dict__["resnet34"](pretrained=None)
-#         self.l0 = nn.Linear(512,n_class)
+class ResNet34(nn.Module):
+    def __init__(self,pretrained, n_class):
+        super(ResNet34, self).__init__()
+        if pretrained is True:
+            self.model = pretrainedmodels.__dict__["resnet34"](pretrained='imagenet')
+        else:
+            self.model = pretrainedmodels.__dict__["resnet34"](pretrained=None)
+        self.l0 = nn.Linear(512,n_class)
 
-#     def forward(self, x):
-#         batch_size, _, _, _ = x.shape
-#         x = self.model.features(x)
-#         x = F.adaptive_avg_pool2d(x, 1).reshape(batch_size,-1)
-#         output = self.l0(x)
+    def forward(self, x):
+        batch_size, _, _, _ = x.shape
+        x = self.model.features(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(batch_size,-1)
+        output = self.l0(x)
 
-#         return output
+        return output
 
 class VGG16(nn.Module):
     def __init__(self,pretrained, n_class):
@@ -37,26 +37,26 @@ class VGG16(nn.Module):
 
         return output
 
-# class SE_ResNext101_32x4d(nn.Module):
-#     def __init__(self,pretrained, n_class):
-#         super(SE_ResNext101_32x4d, self).__init__()
-#         if pretrained is True:
-#             self.model = pretrainedmodels.__dict__["se_resnext101_32x4d"](pretrained='imagenet')
-#         else:
-#             self.model = pretrainedmodels.__dict__["se_resnext101_32x4d"](pretrained=None)
+class SE_ResNext101_32x4d(nn.Module):
+    def __init__(self,pretrained, n_class):
+        super(SE_ResNext101_32x4d, self).__init__()
+        if pretrained is True:
+            self.model = pretrainedmodels.__dict__["se_resnext101_32x4d"](pretrained='imagenet')
+        else:
+            self.model = pretrainedmodels.__dict__["se_resnext101_32x4d"](pretrained=None)
 
-#         self.l0 = nn.Linear(2048,n_class)
+        self.l0 = nn.Linear(2048,n_class)
 
-#     def forward(self, x):
-#         '''
-#         WIP
-#         :param x:
-#         :return:
-#         '''
-#         batch_size, _, _, _ = x.shape
-#         x = self.model.features(x)
-#         x = F.adaptive_avg_pool2d(x, 1).reshape(batch_size,-1)
-#         output = self.l0(x)
+    def forward(self, x):
+        '''
+        WIP
+        :param x:
+        :return:
+        '''
+        batch_size, _, _, _ = x.shape
+        x = self.model.features(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(batch_size,-1)
+        output = self.l0(x)
 
-#         return output
+        return output
 
