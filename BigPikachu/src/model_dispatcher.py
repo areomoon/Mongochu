9bb6a7ad2a8a3e8d06a621fa1b368aac8747128a
Tang-Li-Jen <b00701160@gmail.com>
2020-04-10 23:12:42 +0800

    fix bug

--- BigPikachu/src/model_dispatcher.py
+++ BigPikachu/src/model_dispatcher.py
@@ -4,7 +4,7 @@ MODEL_DISPATCHER = {
     'resnet34': models.ResNet34(pretrained=True, n_class=3),
     'resnet34_eval': models.ResNet34(pretrained=False, n_class=3),
     'vgg16': models.VGG16(pretrained=True, n_class=3),
-    'vgg16_eval': models.VGG16(pretrained=False, n_class=3),
-    'se_resnext101_32x4d':models.SE_ResNext101_32x4d(pretrained=True, n_class=3),
-    'se_resnext101_32x4d_eval':models.SE_ResNext101_32x4d(pretrained=False, n_class=3)
+    'vgg16_eval': models.VGG16(pretrained=False, n_class=3)
+    # 'se_resnext101_32x4d':models.SE_ResNext101_32x4d(pretrained=True, n_class=3),
+    # 'se_resnext101_32x4d_eval':models.SE_ResNext101_32x4d(pretrained=False, n_class=3)
 }
