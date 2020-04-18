93366ea784341bb9dc56fae53859914770017a88
Tang-Li-Jen <b00701160@gmail.com>
2020-04-19 05:28:29 +0800

    clip lam in cutmix

--- BigPikachu/src/train_cutmix_with_sampler.py
+++ BigPikachu/src/train_cutmix_with_sampler.py
@@ -116,7 +116,7 @@ def train(dataset_size, dataloader, model, optimizer, device, loss_fn):
         r = np.random.rand(1)
         if args.beta > 0 and r < args.cutmix_prob:
             # generate mixed sample
-            lam = np.random.beta(args.beta, args.beta)
+            lam = np.clip(np.random.beta(args.beta, args.beta), 0.3, 0.7)
             rand_index = torch.randperm(image.size()[0]).cuda()
             target_a = target
             target_b = target[rand_index]
