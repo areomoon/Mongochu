a6fdb10672a216b30b5481348a40524db24f8773
Tang-Li-Jen <b00701160@gmail.com>
2020-04-19 04:39:34 +0800

    fix sampler is mutually exclusive with shuffle

--- BigPikachu/src/train_cutmix_with_sampler.py
+++ BigPikachu/src/train_cutmix_with_sampler.py
@@ -241,7 +241,6 @@ def main():
     train_dataloader = DataLoader(
         dataset=train_dataset,
         batch_size=args.train_batch_size,
-        shuffle=True,
         num_workers=args.num_workers,
         sampler=train_sampler
     )
@@ -261,7 +260,6 @@ def main():
     valid_dataloader = DataLoader(
         dataset=valid_dataset,
         batch_size=args.test_batch_size,
-        shuffle=False,
         num_workers=args.num_workers,
         sampler=valid_sampler
     )
