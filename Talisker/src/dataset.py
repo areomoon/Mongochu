7fc8d10d72fdb133acd351aef8552dae6fea4540
kunw-ho <kuho@deloitte.com.tw>
2020-05-03 23:50:11 +0800

    ImageExp2Dataset

--- Talisker/src/dataset.py
+++ Talisker/src/dataset.py
@@ -169,12 +169,16 @@ class ImageExp2Dataset:
             # training set
             self.aug = albumentations.Compose([
                 albumentations.Resize(image_height, image_width, always_apply=True),
-                albumentations.RandomShadow(shadow_roi=(0, 0.85, 1, 1), p=0.5),
-                albumentations.RandomBrightnessContrast(brightness_limit=0.10, contrast_limit=0.10, p=0.5),
+                albumentations.HorizontalFlip(p=0.5),
+                albumentations.VerticalFlip(p=0.5),
+                #albumentations.ToGray(always_apply=True),
+                #albumentations.RandomShadow(shadow_roi=(0, 0.85, 1, 1), p=0.5),
+                albumentations.RandomBrightnessContrast(brightness_limit=(0.20,-0.20), contrast_limit=(0.20,-0.20), p=.5),
                 albumentations.ShiftScaleRotate(shift_limit=0.0625,
                                                 scale_limit=0.1,
                                                 rotate_limit=5,
-                                                p=0.9),
+                                                p=0.5),
+                albumentations.InvertImg(p=.5),
                 albumentations.Normalize(mean, std, always_apply=True)
             ])
 
