ed2fd6fbfc5f7240873a7be039b819fba5e9e9ea
Tang-Li-Jen <b00701160@gmail.com>
2020-05-16 16:55:27 +0800

    add flip

--- BigPikachu/src/dataset.py
+++ BigPikachu/src/dataset.py
@@ -230,6 +230,7 @@ class ImageSamplerDataset:
                                                 scale_limit=(0.0, 0.5),
                                                 rotate_limit=0,
                                                 p=0.5),
+                albumentations.Flip(always_apply=False, p=0.4),
                 albumentations.Normalize(mean, std, always_apply=True)
                 # albumentations.ChannelShuffle(always_apply=False, p=0.2),
                 # albumentations.HueSaturationValue(always_apply=False, p=0.5, hue_shift_limit=(-20, 20), sat_shift_limit=(-30, 30), val_shift_limit=(-20, 20))
