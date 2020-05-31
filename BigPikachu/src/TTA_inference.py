30e8fe58d4dc95fe765a1592e7ac9294025839e7
Tang-Li-Jen <b00701160@gmail.com>
2020-05-31 14:23:16 +0800

    rm tofloat and hue aug

--- BigPikachu/src/TTA_inference.py
+++ BigPikachu/src/TTA_inference.py
@@ -59,10 +59,10 @@ data_transforms = albumentations.Compose([
         albumentations.CLAHE(clip_limit=2), albumentations.IAASharpen(), albumentations.IAAEmboss(), 
         albumentations.RandomBrightness(), albumentations.RandomContrast(),
         albumentations.JpegCompression(), albumentations.Blur(), albumentations.GaussNoise()], p=0.5), 
-    albumentations.HueSaturationValue(p=0.5), 
+    # albumentations.HueSaturationValue(p=0.5), 
     albumentations.ShiftScaleRotate(shift_limit=0.15, scale_limit=0.15, rotate_limit=45, p=0.5),
     albumentations.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225]),
-    albumentations.ToFloat()
+    # albumentations.ToFloat()
     ])
 
 data_transforms_test = albumentations.Compose([
